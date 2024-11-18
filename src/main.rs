1d1d8c7df32ab1270a99f57991e1a33299b79fc1
LeeLevi <pol_1987@163.com>
2024-11-18 18:29:57 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+// !main.rs
+use zero2prod::run;
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(||{
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+async fn main() -> std::io::Result<()>
+{
+    run().await
 }

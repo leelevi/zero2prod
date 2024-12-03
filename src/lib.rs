f16d96640c390043003eac1bd549acc7e28de1d6
LeeLevi <pol_1987@163.com>
2024-12-03 11:07:30 +0800

    更新

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,13 @@ use std::net::TcpListener;
 // !lib.rs
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
-async fn health_check(_req: HttpRequest) -> impl Responder {
+async fn health_check()-> HttpResponse{
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe()-> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 pub fn run(listener: TcpListener) -> Result<Server,std::io::Error> {
     let server = HttpServer::new(|| {
         App::new().route("/health_check", web::get().to(health_check))
